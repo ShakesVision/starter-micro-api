e3048250f773f81d64a689865809f6faa523edff
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-12-22 02:08:44 +0530

    new: use fetch api for req

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    const websiteUrl = "http://live.bazm.org:9002/live";
+    fetch(websiteUrl).then((res) => {
+        const statusCode = res.statusCode;
+        res.write(statusCode);
+    }).catch((err) => console.log(err));
+    res.write('\nYo!');
     res.end();
 }).listen(process.env.PORT || 3000);
