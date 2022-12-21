05065671ddc757f6f5e05b05315975f8ee6d665c
Shakeeb Ahmad <shakeeb.ahmad@simplifiedautomation.ca>
2022-12-22 02:16:36 +0530

    more changes

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     const websiteUrl = "http://live.bazm.org:9002/live";
-    fetch(websiteUrl).then((res) => {
+    http.get(websiteUrl, (res) => {
         const statusCode = res.statusCode;
-        res.write(statusCode);
-    }).catch((err) => console.log(err));
+        res.write("Status: " + statusCode);
+    }).on("error", (error) => {
+        res.write(500);
+        res.sendStatus(500)
+        console.log(error);
+    });
     res.write('\nYo!');
     res.end();
 }).listen(process.env.PORT || 3000);
